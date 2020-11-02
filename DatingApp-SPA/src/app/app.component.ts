1d31bda3b7d80a92a5e8fef51b2850cfd64b54e1
agenna@hotmail.it <agenna@hotmail.it>
2020-11-02 18:33:01 +0100

    end of section 7

--- DatingApp-SPA/src/app/app.component.ts
+++ DatingApp-SPA/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { AuthService } from './_services/auth.service';
 
@@ -8,7 +8,7 @@ import { AuthService } from './_services/auth.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  //title = 'DatingApp-SPA';
+
   jwtHelper = new JwtHelperService();
 
   constructor(private authService: AuthService){}
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -11,7 +12,8 @@ export class NavComponent implements OnInit {
 
   model: any = {};
   constructor(public authService: AuthService,
-              private alertify: AlertifyService) { }
+              private alertify: AlertifyService,
+              private route: Router) { }
 
   ngOnInit(): void {
   }
@@ -20,7 +22,9 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model)
     .subscribe(
       () => {
-        this.alertify.success('Logged successfully'); },
+        this.alertify.success('Logged successfully');
+        this.route.navigate(['/members']);
+      },
       err => {
         this.alertify.error(err); }
     );
@@ -34,5 +38,6 @@ export class NavComponent implements OnInit {
   logout(): void{
     localStorage.removeItem('token');
     this.alertify.message('logged out');
+    this.route.navigate(['/home']);
   }
 }
